46ebbb0e3cbdeab2f6428bc0fa0bd825443999cd
mambo1991@163.com <wangbao1991>
2017-11-27 19:04:25 +0800

    11月27日 第四篇：断路器。Ribbon中使用断路器

--- service-ribbon/src/main/java/com/h3c/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/h3c/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.h3c.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +18,19 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    /**
+     * 改造HelloService类，在hiService方法上加上@HystrixCommand注解。该注解对该方法创建了熔断器的功能，
+     * 并指定了fallbackMethod熔断方法，熔断方法直接返回了一个字符串，
+     * 字符串为”hi,”+name+”,sorry,error!”，代码如下：
+     * @param name
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + "sorry, error!";
     }
 }
