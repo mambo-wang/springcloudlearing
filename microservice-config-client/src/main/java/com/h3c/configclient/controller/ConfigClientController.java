6e2572cb36a81d138cc05559cefa78bf81f78bd1
mambo-wang <mambo1991@163.com>
2018-02-05 15:21:47 +0800

    2月5日 Config Server手动刷新配置

--- microservice-config-client/src/main/java/com/h3c/configclient/controller/ConfigClientController.java
+++ microservice-config-client/src/main/java/com/h3c/configclient/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.h3c.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //添加该注解的类会在配置更改时得到特殊的处理
 public class ConfigClientController {
 
     /** 绑定Git仓库配置文件中的profile属性 */
